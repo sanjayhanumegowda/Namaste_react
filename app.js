63ec6fabf37a45adb15f954e49363a8555bdea75
Sanjay H P <snj.sanju@gmail.com>
2024-07-24 07:29:39 +0530

    episode-1

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const parent = React.createElement(
+  'div',{id:'parent'},
+  [
+    React.createElement('div',{id:'child1'},
+      [
+        React.createElement('h1',{},'heading1'),
+        React.createElement('h2',{},'heading2')
+      ]),
+    React.createElement('div',{id:'child2'},[
+      React.createElement('h1',{},'heading1'),
+      React.createElement('h2',{},'heading2')
+    ])
+  ]);
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(parent);
